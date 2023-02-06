97bd698e48884ee471aa567f1069f41e7e869c2c
foucauld <foucauldbeaumont@yahoo.fr>
2023-02-06 15:24:20 +0100

    aa

---
+++ src/api/posts/routes/posts.js
@@ -0,0 +1,13 @@
+module.exports = {
+  routes: [
+    // {
+    //  method: 'GET',
+    //  path: '/posts',
+    //  handler: 'posts.exampleAction',
+    //  config: {
+    //    policies: [],
+    //    middlewares: [],
+    //  },
+    // },
+  ],
+};
